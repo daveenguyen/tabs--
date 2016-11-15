2cbc5fbb5e511249b100b9d07695ad5d1f5ada88
Davee Nguyen <davee@daveenguyen.com>
2016-11-14 20:57:04 -0800

    Use storage to store snoozed tabs

--- event/src/index.js
+++ event/src/index.js
@@ -7,92 +7,107 @@ const middleware = [
   alias(aliases),
 ];
 
-const store = createStore(
-  rootReducer,
-  {},
-  applyMiddleware(...middleware)
-);
+chrome.storage.local.get('storeState', (item) => {
+  let initialState = { snoozedTabs: item.storeState } || {};
+  initializeStore(initialState);
+});
 
+const initializeStore = (initialState) => {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middleware)
+  );
 
-let unsnoozeSet = new Set();
+  const saveChanges = () => {
+    chrome.storage.local.set({
+      'storeState': store.getState().snoozedTabs
+    }, () => {
 
-const setupUnsnooze = (tabs) => {
-
-  if (Object.keys(tabs) && Object.keys(tabs).length > 0) {
-    Object.keys(tabs).map((key) => {
-
-      let tab = tabs[key];
-
-      if (!unsnoozeSet.has(key)) {
-        unsnoozeSet.add(key);
-        console.log(tab);
-
-        let timeout = tab.snoozeUntil - Date.now();
-
-        if (timeout < 0) timeout = 0;
-
-        setTimeout(() => {
-            store.dispatch({
-              type: 'unsnooze',
-              payload: tab
-            });
-            unsnoozeSet.delete(key);
-          }
-        , timeout);
-      }
     });
   }
+
+  let unsnoozeSet = new Set();
+
+  const setupUnsnooze = (tabs) => {
+
+    if (Object.keys(tabs) && Object.keys(tabs).length > 0) {
+      Object.keys(tabs).map((key) => {
+
+        let tab = tabs[key];
+
+        if (!unsnoozeSet.has(key)) {
+          unsnoozeSet.add(key);
+
+          let timeout = tab.snoozeUntil - Date.now();
+
+          if (timeout < 2000) timeout = 2000;
+
+          setTimeout(() => {
+              store.dispatch({
+                type: 'unsnooze',
+                payload: tab
+              });
+              unsnoozeSet.delete(key);
+            }
+          , timeout);
+        }
+      });
+    }
+  }
+
+  store.subscribe(() => {
+    setupUnsnooze(store.getState().snoozedTabs);
+    saveChanges();
+  })
+
+
+  chrome.tabs.query({}, (tabs) => {
+    tabs.map((tab) => {
+      store.dispatch({
+        type: 'ADD_TAB',
+        payload: tab
+      });
+    });
+  });
+
+  chrome.tabs.onReplaced.addListener((addedId, removedId) => {
+    store.dispatch({
+      type: 'REMOVE_TAB',
+      payload: removedId
+    });
+
+    chrome.tabs.get(addedId, (tab) => {
+      store.dispatch({
+        type: 'ADD_TAB',
+        payload: tab
+      });
+    });
+  });
+
+  chrome.tabs.onCreated.addListener((tab) => {
+    store.dispatch({
+      type: 'ADD_TAB',
+      payload: tab
+    });
+  });
+
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    store.dispatch({
+      type: 'ADD_TAB',
+      payload: tab
+    });
+  });
+
+  chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    store.dispatch({
+      type: 'REMOVE_TAB',
+      payload: tabId
+    });
+  });
+
+  wrapStore(store, {
+    portName: 'example'
+  });
+
 }
-
-store.subscribe(() => {
-  setupUnsnooze(store.getState().snoozedTabs);
-})
-
-
-chrome.tabs.query({}, (tabs) => {
-  tabs.map((tab) => {
-    store.dispatch({
-      type: 'ADD_TAB',
-      payload: tab
-    });
-  });
-});
-
-chrome.tabs.onReplaced.addListener((addedId, removedId) => {
-  store.dispatch({
-    type: 'REMOVE_TAB',
-    payload: removedId
-  });
-
-  chrome.tabs.get(addedId, (tab) => {
-    store.dispatch({
-      type: 'ADD_TAB',
-      payload: tab
-    });
-  });
-});
-
-chrome.tabs.onCreated.addListener((tab) => {
-  store.dispatch({
-    type: 'ADD_TAB',
-    payload: tab
-  });
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  store.dispatch({
-    type: 'ADD_TAB',
-    payload: tab
-  });
-});
-
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  store.dispatch({
-    type: 'REMOVE_TAB',
-    payload: tabId
-  });
-});
-
-wrapStore(store, {
-  portName: 'example'
-});
